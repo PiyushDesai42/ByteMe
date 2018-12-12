4e5ca85cadaa17a66fd1f7b476a1b2ef279414db
8onlichtman <8onlichtman@gmail.com>
2018-12-12 10:44:08 +0200

    Read image
    Fixing image reading in map.

--- gis_game/src/game_elements/Map.java
+++ gis_game/src/game_elements/Map.java
@@ -6,26 +6,33 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 
 import Coords.Lat_lon_alt;
 
 public class Map {
 
-//	private Image image;
+	BufferedImage image = null;
 	private Lat_lon_alt gps_min, gps_max;
 
-	public Map(String input, Lat_lon_alt min, Lat_lon_alt max) {
-//		image = new Image();
+	public Map(String img, Lat_lon_alt min, Lat_lon_alt max) {
+		try {
+		    image = ImageIO.read(new File(img));
+		} catch (IOException e) {
+			System.out.println("could not load image!");
+		}
 		gps_min = new Lat_lon_alt(min);
 		gps_max = new Lat_lon_alt(max);
 	}
 
-//	public static Point gps_to_pixel(Lat_lon_alt gps) {
+//	public Point gps_to_pixel(Lat_lon_alt gps) {
 //
 //	}
 //
-//	public static Lat_lon_alt pixel_to_gps(Point pixel) {
+//	public Lat_lon_alt pixel_to_gps(Point pixel) {
 //
 //	}
 }
