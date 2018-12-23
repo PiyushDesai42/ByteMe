69f58fdfdd400453be47cad17957d323d5550bac
8onlichtman <8onlichtman@gmail.com>
2018-12-23 23:00:23 +0200

    Algorithm
    Fixing bug in Algorithm.

--- gis_game/src/algo/Algorithm.java
+++ gis_game/src/algo/Algorithm.java
@@ -23,36 +23,43 @@ public class Algorithm {
 	public static void run_algo(Game game, ArrayList<Path> paths) {
 
 		ArrayList<Fruit> fruits = new ArrayList<Fruit>(game.getFruits());
+		ArrayList<Packman> packmans = new ArrayList<Packman>();
 		Iterator<Packman> it_packman = game.getPackmans().iterator();
 		while(it_packman.hasNext()) {
-			paths.add(new Path(it_packman.next()));
+			Packman p = it_packman.next();
+			paths.add(new Path(p));
+			packmans.add(new Packman(p));
 		}
-
-
 
 		while(!fruits.isEmpty()) {
 			Fruit closest_fruit = null;
 			double closest_fruit_length = Integer.MAX_VALUE;
 			Path best_path = null;
+			Packman best_packman = null;
+			it_packman = packmans.iterator();
 			Iterator<Path> it_path = paths.iterator();
-			while(it_path.hasNext()) {
+			while(it_path.hasNext() && it_packman.hasNext()) {
 				Path current_path = it_path.next();
+				Packman current_packman = it_packman.next();
 				Iterator<Fruit> it_fruit = fruits.iterator();
 				Fruit current_fruit=null;
 				while(it_fruit.hasNext()) {
 					current_fruit = it_fruit.next();
-					if(current_path.getTime()+seconds_to_fruit(current_path.getPackman(), current_fruit)
+					if(current_path.getTime()+seconds_to_fruit(current_packman, current_fruit)
 					<closest_fruit_length) {
+						best_packman = current_packman;
 						best_path = current_path;
 						closest_fruit = current_fruit;
-						closest_fruit_length = current_path.getTime()+seconds_to_fruit(current_path.getPackman(), current_fruit);
+						closest_fruit_length = current_path.getTime()+seconds_to_fruit(current_packman, current_fruit);
 					}
 				}
 			}
 			best_path.add_fruit(closest_fruit);
 			fruits.remove(closest_fruit);
 			best_path.setTime(closest_fruit_length);
+			best_packman.setGps_point(closest_fruit.getGps_point());
 		}
+		
 	}
 
 
